2d9d1c5df0795ca6a1acc9c95bcd54464478669f
Cristhian de Almeida <cristhian.almeida@certsys.com.br>
2020-01-20 19:23:47 -0300

    Criação Controller Empresa

---
+++ PontoEletronicoApi/Controllers/EmpresaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PontoEletronico.Data;
+using PontoEletronico.Servico.Base;
+
+namespace PontoEletronicoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ControllerBase
+    {
+        private readonly BaseServico servico;
+        private readonly IMapper _mapper;
+        public EmpresaController(ApplicationDbContext ctx,IMapper mapper)
+        {
+            servico = new BaseServico(ctx);
+            _mapper = mapper;
+        }
+
+
+    }
+}
